fd48293ac289eac92b800eca1d9d2b00aaa1a38b
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-02-11 18:00:20 +0900

    fix: setGrade 초기화 누락 수정

--- models/subject.js
+++ models/subject.js
@@ -1,66 +1,66 @@
- module.exports = class Subject {
+module.exports = class Subject {
     constructor(build) {
         if (build) {
-          this.id = build.id;
-          this.name = build.name;
-          this.professor = build.professor;
-          this.room = build.room;
-          this.type = build.type;
-          this.inwon = build.inwon;
-          this.grade = build.grade;
-          this.detail = build.detail;
+            this.id = build.id;
+            this.name = build.name;
+            this.professor = build.professor;
+            this.room = build.room;
+            this.type = build.type;
+            this.inwon = build.inwon;
+            this.grade = build.grade;
+            this.detail = build.detail;
         }
-      }
+    }
 
-      static get Builder() {
+    static get Builder() {
         class Builder {
-    
-          setId(id) {
-            this.id = id;
-            return this;
-          }
-    
-          setName(name) {
-            this.name = name;
-            return this;
-          }
-    
-          setProfessor(professor) {
-            this.professor = professor;
-            return this;
-          }
 
-          setRoom(room) {
-            this.room = room;
-            return this;
-          }
+            setId(id) {
+                this.id = id;
+                return this;
+            }
 
-          setType(type) {
-            this.type = type;
-            return this;
-          }
+            setName(name) {
+                this.name = name;
+                return this;
+            }
 
-          setInwon(inwon) {
-            this.inwon = inwon;
-            return this;
-          }
+            setProfessor(professor) {
+                this.professor = professor;
+                return this;
+            }
 
-          setGrade(grade) {
-            this.grade;
-            return this;
-          }
+            setRoom(room) {
+                this.room = room;
+                return this;
+            }
 
-          setDetail(detail) {
-            this.detail = detail;
-            return this;
-          }
-    
-          build() {
-            return new Subject(this);
-          }
+            setType(type) {
+                this.type = type;
+                return this;
+            }
+
+            setInwon(inwon) {
+                this.inwon = inwon;
+                return this;
+            }
+
+            setGrade(grade) {
+                this.grade = grade;
+                return this;
+            }
+
+            setDetail(detail) {
+                this.detail = detail;
+                return this;
+            }
+
+            build() {
+                return new Subject(this);
+            }
         }
-    
+
         return new Builder();
-      }
-    
+    }
+
 }
