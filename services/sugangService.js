e5350d98e9db06355b44c339b29a7217bb9ea4ca
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-02-11 18:03:28 +0900

    Refactor: departmentService -> sugangService

--- services/sugangService.js
+++ services/sugangService.js
@@ -56,8 +56,8 @@ async function login() {
     });
 }
 
-async function find(departmentId) {
-    var subjects;
+async function find(departmentId, type, grade) {
+    let subjects;
     await fetch("https://kuis.konkuk.ac.kr/CourTotalTimetableInq/find.do", {
         "headers": {
             "accept": "*/*",
@@ -75,11 +75,14 @@ async function find(departmentId) {
             "Referrer-Policy": "strict-origin-when-cross-origin",
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
         },
-        "body": `Oe2Ue=%239e4ki&Le093=e%26*%08iu&AWeh_3=W%5E_zie&Hd%2Cpoi=_qw3e4&EKf8_%2F=Ajd%25md&WEh3m=ekmf3&rE%0Cje=JDow871&JKGhe8=NuMoe6&_)e7me=ne%2B3%7Cq&3kd3Nj=Qnd%40%251&_AUTH_MENU_KEY=1130420&%40d1%23ltYy=2022&%40d1%23ltShtm=B01011&%40d1%23openSust=${departmentId}&%40d1%23pobtDiv=&%40d1%23sbjtId=&%40d1%23corsKorNm=&%40d1%23sprfNo=&%40d1%23argDeptFg=1&%40d1%23arglangNm=&%40d%23=%40d1%23&%40d1%23=dmParam&%40d1%23tp=dm&`,
+        "body": `Oe2Ue=%239e4ki&Le093=e%26*%08iu&AWeh_3=W%5E_zie&Hd%2Cpoi=_qw3e4&EKf8_%2F=Ajd%25md&WEh3m=ekmf3&rE%0Cje=JDow871&JKGhe8=NuMoe6&_)e7me=ne%2B3%7Cq&3kd3Nj=Qnd%40%251&_AUTH_MENU_KEY=1130420&%40d1%23ltYy=2022&%40d1%23ltShtm=B01011&%40d1%23openSust=${departmentId}&%40d1%23pobtDiv=${type}&%40d1%23sbjtId=&%40d1%23corsKorNm=&%40d1%23sprfNo=&%40d1%23argDeptFg=1&%40d1%23arglangNm=&%40d%23=%40d1%23&%40d1%23=dmParam&%40d1%23tp=dm&`,
         "method": "POST"
-    }).then((res) => res.json())
+    }).then((res) => {
+        console.log(typeof res);
+        return res.json();
+    })
         .then(async (res) => {
-            subjects = (await Promise.all(res.DS_SUSTTIMETABLE.map(async (subjectInfo) => {
+            subjects = (await Promise.all(res.DS_SUSTTIMETABLE.filter(subjectInfo => grade.includes(subjectInfo.OPEN_SHYR)).map(async (subjectInfo) => {
                 const subject = Subject.Builder
                     .setId(subjectInfo.SBJT_ID)
                     .setName(subjectInfo.TYPL_KOR_NM)
@@ -93,18 +96,90 @@ async function find(departmentId) {
                 return subject;
             })
             ))
-            // console.log(subjects);
+            console.log(subjects);
         });
-        return subjects;
+    return subjects;
 }
 
-exports.getSubjects = async function (departmentId) {
-    var result;
-    console.time('getSubjects')
+async function findBySubjectId(subjectId) {
+    let subjects;
+    await fetch("https://kuis.konkuk.ac.kr/CourTotalTimetableInq/find.do", {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ny;q=0.6",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+            "cookie": `WMONID=MW_vHzajhWZ; JSESSIONID=${jSessionId}`,
+            "Referer": "https://kuis.konkuk.ac.kr/index.do",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
+        },
+        "body": `Oe2Ue=%239e4ki&Le093=e%26*%08iu&AWeh_3=W%5E_zie&Hd%2Cpoi=_qw3e4&EKf8_%2F=Ajd%25md&WEh3m=ekmf3&rE%0Cje=JDow871&JKGhe8=NuMoe6&_)e7me=ne%2B3%7Cq&3kd3Nj=Qnd%40%251&_AUTH_MENU_KEY=1130420&%40d1%23ltYy=2022&%40d1%23ltShtm=B01011&%40d1%23openSust=&%40d1%23pobtDiv=&%40d1%23sbjtId=${subjectId}&%40d1%23corsKorNm=&%40d1%23sprfNo=&%40d1%23argDeptFg=1&%40d1%23arglangNm=&%40d%23=%40d1%23&%40d1%23=dmParam&%40d1%23tp=dm&`,
+        "method": "POST"
+    }).then((res) => {
+        console.log(typeof res);
+        return res.json();
+    })
+        .then(async (res) => {
+            subjects = await Promise.all(res.DS_SUSTTIMETABLE.map(async (subjectInfo) => {
+                const subject = Subject.Builder
+                    .setId(subjectInfo.SBJT_ID)
+                    .setName(subjectInfo.TYPL_KOR_NM)
+                    .setProfessor(subjectInfo.KOR_NM)
+                    .setRoom(subjectInfo.ROOM_NM)
+                    .setType(subjectInfo.POBT_DIV_NM)
+                    .setInwon(subjectInfo.TLSN)
+                    .setGrade(subjectInfo.OPEN_SHYR)
+                    .setDetail(subjectInfo.REMK)
+                    .build();
+                return subject;
+            })
+            )
+            console.log(subjects);
+        });
+    return subjects;
+}
+
+exports.getSubjects = async function (departmentId, type, grade) {
+    let result;
+    console.time('getSubjects');
     await index()
         .then(async () => await login())
         .then(async () => {
-            result =  await find(departmentId);
-        })
+            result = await find(departmentId, type, grade);
+        });
+    console.timeEnd('getSubjects');
+    return result;
+}
+
+exports.getSubject = async function (subjectId) {
+    let result;
+    console.time('getSubject');
+    await index()
+        .then(async () => await login())
+        .then(async () => {
+            result = await findBySubjectId(subjectId);
+        });
+    console.timeEnd('getSubject');
+    return result;
+}
+
+exports.getMySubjectList = async function (mySubjectList) {
+    let result;
+    console.time('getMySubjectList');
+    await index()
+        .then(async () => await login())
+        .then(async () => {
+            result = await Promise.all(mySubjectList.map(async (subjectId) => {
+                return await findBySubjectId(subjectId);
+            }))
+        });
+    console.timeEnd('getMySubjectList');
     return result;
 }
