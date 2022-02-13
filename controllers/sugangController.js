bf77084c3c166bab6ed9288d57ead596f5449fcb
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-02-13 21:16:46 +0900

    Feat: 예외 처리 및 로깅 추가

--- controllers/sugangController.js
+++ controllers/sugangController.js
@@ -1,6 +1,8 @@
 const sugangService = require('../services/sugangService.js');
+const {logger} = require("../config/winston");
 
 exports.getSubjects = async function (req, res) {
+    logger.info('GET: /subjects, QUERY: ' +  JSON.stringify(req.query));
     let departmentId = req.query.departmentId;
     let type = req.query.type;
     let grade = req.query.grade;
@@ -13,19 +15,32 @@ exports.getSubjects = async function (req, res) {
     if (!grade) {
         grade = ["1", "2", "3", "4", "9"];
     }
+    try{
     const result = await sugangService.getSubjects(departmentId, type, grade);
-    return res.send(JSON.stringify(result));
+    return res.status(200).send(JSON.stringify(result));
+    } catch(e) {
+        return res.status(404).send();
+    }
 };
 
 exports.getSubject = async function (req, res) {
+    logger.info('GET: /subject/, Params: ' +  req.params.subjectId);
     const subjectId = req.params.subjectId;
+    try{
     const result = await sugangService.getSubject(subjectId);
-    return res.send(JSON.stringify(result));
+    return res.status(200).send(JSON.stringify(result));
+    } catch (e) {
+        return res.status(404).send();
+    }
 };
 
 exports.getMySubjectList = async function (req, res) {
-    console.log(req.body.sbj_num);
+    logger.info('POST: /subjects, : ' +  JSON.stringify(req.body));
     const mySubjectList = req.body.sbj_num;
+    try {
     const result = await sugangService.getMySubjectList(mySubjectList);
-    return res.send(JSON.stringify(result));
+    return res.status(200).send(JSON.stringify(result));
+    } catch(e){
+        return res.status(404).send();
+    }
 }
